ea7e71b14329e1ebd056365ee417c57211440d51
bojanilijsoki <b.ilijoski@gmail.com>
2014-03-09 13:56:16 +0100

    player movement

--- game/ui/screens/game_screen.js
+++ game/ui/screens/game_screen.js
@@ -23,13 +23,40 @@
         this.is_left = false;
         this.is_right = false;
         
+        this.player_speed = 1;
+        
+        var that = this;
         
         this.kibo.down(['up'], function() {
-           this.is_up = true;
+           that.is_up = true;
         });
         
         this.kibo.up(['up'], function() {
-           this.is_up = false;
+           that.is_up = false;
+        });
+        
+        this.kibo.down(['down'], function() {
+           that.is_down = true;
+        });
+        
+        this.kibo.up(['down'], function() {
+           that.is_down = false;
+        });
+        
+         this.kibo.down(['left'], function() {
+           that.is_left = true;
+        });
+        
+        this.kibo.up(['left'], function() {
+           that.is_left = false;
+        });
+        
+        this.kibo.down(['right'], function() {
+           that.is_right = true;
+        });
+        
+        this.kibo.up(['right'], function() {
+           that.is_right = false;
         });
         
     };   
@@ -40,7 +67,70 @@
 
     GameScreen.prototype.update = function() {
 
+        this.update_movement();
+     
+
+    };
     
+    GameScreen.prototype.update_movement = function() {
+        
+         if(this.is_right && this.is_down)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x+this.player_speed,y:p.y+this.player_speed});
+            return;
+        }
+        
+        if(this.is_right && this.is_up)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x+this.player_speed,y:p.y-this.player_speed});
+            return;
+        }
+        
+        if(this.is_left && this.is_down)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x-this.player_speed,y:p.y+this.player_speed});
+            return;
+        }
+        
+        if(this.is_left && this.is_up)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x-this.player_speed,y:p.y-this.player_speed});
+            return;
+        }
+
+        if(this.is_down)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x,y:p.y+this.player_speed});
+            return;
+        }
+        
+        if(this.is_up)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x,y:p.y-this.player_speed});
+            return;
+        }
+        
+        if(this.is_left)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x-this.player_speed,y:p.y});
+            return;
+        }
+        
+        if(this.is_right)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x:p.x+this.player_speed,y:p.y});
+            return;
+        }
+        
+       
      
 
     };
