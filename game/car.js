4b87e1cf04a0b9cfd4a33a9fb65bc6ad75d3c58d
bojanilijsoki <b.ilijoski@gmail.com>
2014-03-15 12:57:08 +0100

    cars movement and direction

--- game/car.js
+++ game/car.js
@@ -1,77 +1,83 @@
-(function(window,undefined){
-    
-    function Car(){
+(function(window, undefined) {
+
+    function Car() {
         this.initialize();
-    }    
-    
+    }
+
     Car.prototype = new Animation();
-    Car.prototype.animation_initialize = Car.prototype.initialize;    
-    Car.prototype.initialize = function(){        
-        
+    Car.prototype.animation_initialize = Car.prototype.initialize;
+    Car.prototype.initialize = function() {
+
         var sprite_sheet = new SpriteSheet([{
-                    image: ContentManager.images.car_animation,
-                    frames: {x:2,y:1},
-                    animations: {
-                        drive: {start:0,end:1,loop:true,duration:300}                         
-                    }
-                    ,reg:{x:0.5,y:0.5,width:1.0,height:1.0}
-                }]);
-        
+                image: ContentManager.images.car_animation,
+                frames: {x: 2, y: 1},
+                animations: {
+                    drive: {start: 0, end: 1, loop: true, duration: 300}
+                }
+                , reg: {x: 0.5, y: 0.5, width: 1.0, height: 1.0}
+            }]);
+
         this.animation_initialize(sprite_sheet);
-        
-       // this.image = ContentManager.images.car.image;
-        
-        this.start_position={x:0,y:0};
-        this.end_position={x:0,y:0};
-        this.speed = 100/1000;
-        
-        
+
+        // this.image = ContentManager.images.car.image;
+
+        this.start_position = {x: 0, y: 0};
+        this.end_position = {x: 0, y: 0};
+        this.speed = 100 / 1000;
+        this.angle = 0;
+
+        this.callback = function() {
+        };
     };
-    
-    Car.prototype.on_added_to_parent = function(parent){
-        Animation.prototype.on_added_to_parent.call(this,parent);
+
+    Car.prototype.on_added_to_parent = function(parent) {
+        Animation.prototype.on_added_to_parent.call(this, parent);
         this.play("drive");
     };
-    
-    Car.prototype.on_remove_from_parent = function(parent){
-        Animation.prototype.on_remove_from_parent.call(this,parent);
-        
+
+    Car.prototype.on_remove_from_parent = function(parent) {
+        Animation.prototype.on_remove_from_parent.call(this, parent);
+
     };
-    
-    Car.prototype.draw = function(context){
-        
-        // CALL THE PARENT DRAW METHOD when overwriting
-        Animation.prototype.draw.call(this,context);
-        
-//        var p = this.bounds.position;
-//        
-//        context.drawImage(this.image,p.x,p.y);
-        
+
+    Car.prototype.draw = function(context) {
+
+        context.save();
+
+        var center = this.bounds.get_center();
+        var xx = center.x;
+        context.translate(xx, center.y);
+        context.rotate((this.angle + 90) * Math.PI / 180);
+        context.translate(-xx, -center.y);
+        Animation.prototype.draw.call(this, context);
+
+        context.restore();
+
     };
-    
-    Car.prototype.clear = function(context){
-        
+
+    Car.prototype.clear = function(context) {
+
     };
-    
-    Car.prototype.move = function(){
-      var p = this.get_position();
-      var angle = Math.get_angle(this.start_position, this.end_position);
-      var distance = this.speed*Ticker.step;
-      var next = Math.get_next_point(p, distance, angle);
-      
-      var total_distance = Math.get_distance(this.start_position, this.end_position);
-      var current_distance = Math.get_distance(this.start_position, next);
-      
-      if(current_distance<=total_distance)
-      {
-        this.set_position(next);
-      }
-      else
-      {
-          
-      }
+
+    Car.prototype.move = function() {
+        var p = this.get_position();
+        var angle = this.angle = Math.get_angle(this.start_position, this.end_position);
+        var distance = this.speed * Ticker.step;
+        var next = Math.get_next_point(p, distance, angle);
+
+        var total_distance = Math.get_distance(this.start_position, this.end_position);
+        var current_distance = Math.get_distance(this.start_position, next);
+
+        if (current_distance <= total_distance)
+        {
+            this.set_position(next);
+        }
+        else
+        {
+            this.callback(this);
+        }
     };
-    
+
     window.Car = Car;
-    
+
 }(window));
--- game/ui/screens/game_screen.js
+++ game/ui/screens/game_screen.js
@@ -8,77 +8,132 @@
     GameScreen.prototype.screen_initialize = GameScreen.prototype.initialize;
 
     GameScreen.prototype.initialize = function() {
-        this.screen_initialize();     
-        
+        this.screen_initialize();
+
         this.back_image = ContentManager.images.parking.image;
-        
+
         this.player = new Player();
-        
-        this.player.set_position({x:100,y:200});
-        
+
+        this.player.set_position({x: 100, y: 200});
+
+        this.car_size;
+
         this.add_child(this.player);
-        
+
         this.kibo = new Kibo();
-        
+
         this.is_down = false;
         this.is_up = false;
         this.is_left = false;
         this.is_right = false;
-        
+
         this.player_speed = 1;
-        
+
         var that = this;
-        
+
         this.kibo.down(['up'], function() {
-           that.is_up = true;
+            that.is_up = true;
         });
-        
+
         this.kibo.up(['up'], function() {
-           that.is_up = false;
+            that.is_up = false;
         });
-        
+
         this.kibo.down(['down'], function() {
-           that.is_down = true;
+            that.is_down = true;
         });
-        
+
         this.kibo.up(['down'], function() {
-           that.is_down = false;
+            that.is_down = false;
         });
-        
-         this.kibo.down(['left'], function() {
-           that.is_left = true;
+
+        this.kibo.down(['left'], function() {
+            that.is_left = true;
         });
-        
+
         this.kibo.up(['left'], function() {
-           that.is_left = false;
+            that.is_left = false;
         });
-        
+
         this.kibo.down(['right'], function() {
-           that.is_right = true;
+            that.is_right = true;
         });
-        
+
         this.kibo.up(['right'], function() {
-           that.is_right = false;
+            that.is_right = false;
         });
-        
+
         //cars
         this.cars = [];
-        
-        for(i=0;i<3;i++)
+
+        for (i = 0; i < 3; i++)
         {
             var car = new Car();
-            
-            car.start_position = {x:100,y:100};
-            car.end_position = {x:100*i,y:100*i};
-        
+            car.callback = GameScreen.prototype.car_callback.bind(this);
+
+            this.car_size = Math.sqrt(Math.pow(car.bounds.width, 2) + Math.pow(car.bounds.height, 2));
+
+
+            var points = this.generate_random_points();
+
+            car.start_position = points[0];
+            car.end_position = points[1];
+
             car.set_position(car.start_position);
-        
+
             this.add_child(car);
-            
+
             this.cars.push(car);
         }
+
+    };
+
+    GameScreen.prototype.generate_random_points = function()
+    {
+        var points = [];
+        var start_side = Math.random_in_range(0, 3);
+        var end_side = Math.random_in_range(0, 3);
+        while (start_side == end_side)
+        {
+            end_side = Math.random_in_range(0, 3);
+        }
+
+        if (start_side == 0 || end_side == 0)
+        {
+            points.push({x: Config.screen_width + this.car_size,
+                y: Math.random_in_range(-this.car_size, Config.screen_height + this.car_size)});
+        }
+
+        if (start_side == 1 || end_side == 1)
+        {
+            points.push({x: Math.random_in_range(-this.car_size, Config.screen_width + this.car_size),
+                y: Config.screen_height + this.car_size});
+        }
+
+        if (start_side == 2 || end_side == 2)
+        {
+            points.push({x: -this.car_size,
+                y: Math.random_in_range(-this.car_size, Config.screen_height + this.car_size)});
+        }
+
+        if (start_side == 3 || end_side == 3)
+        {
+            points.push({x: Math.random_in_range(-this.car_size, Config.screen_width + this.car_size),
+                y: -this.car_size});
+        }
+
+        return points;
+    }
+
+    GameScreen.prototype.car_callback = function(car)
+    {
+        var points = this.generate_random_points();
+
+        car.start_position = points[0];
+        car.end_position = points[1];
         
-    };   
+        car.set_position(car.start_position);
+    };
 
     GameScreen.prototype.game_over = function() {
         this.is_game_over = true;
@@ -87,78 +142,78 @@
     GameScreen.prototype.update = function() {
 
         this.update_movement();
-        
-        for(var i in this.cars)
+
+        for (var i in this.cars)
         {
             this.cars[i].move();
         }
-     
+
     };
-    
-    GameScreen.prototype.draw = function(context){
-        
-        context.drawImage(this.back_image,0,0);
-        
+
+    GameScreen.prototype.draw = function(context) {
+
+        context.drawImage(this.back_image, 0, 0);
+
     };
-    
+
     GameScreen.prototype.update_movement = function() {
-        
-         if(this.is_right && this.is_down)
+
+        if (this.is_right && this.is_down)
         {
             var p = this.player.get_position();
-            this.player.set_position({x:p.x+this.player_speed,y:p.y+this.player_speed});
-            return;
-        }
-        
-        if(this.is_right && this.is_up)
-        {
-            var p = this.player.get_position();
-            this.player.set_position({x:p.x+this.player_speed,y:p.y-this.player_speed});
-            return;
-        }
-        
-        if(this.is_left && this.is_down)
-        {
-            var p = this.player.get_position();
-            this.player.set_position({x:p.x-this.player_speed,y:p.y+this.player_speed});
-            return;
-        }
-        
-        if(this.is_left && this.is_up)
-        {
-            var p = this.player.get_position();
-            this.player.set_position({x:p.x-this.player_speed,y:p.y-this.player_speed});
+            this.player.set_position({x: p.x + this.player_speed, y: p.y + this.player_speed});
             return;
         }
 
-        if(this.is_down)
+        if (this.is_right && this.is_up)
         {
             var p = this.player.get_position();
-            this.player.set_position({x:p.x,y:p.y+this.player_speed});
+            this.player.set_position({x: p.x + this.player_speed, y: p.y - this.player_speed});
             return;
         }
-        
-        if(this.is_up)
+
+        if (this.is_left && this.is_down)
         {
             var p = this.player.get_position();
-            this.player.set_position({x:p.x,y:p.y-this.player_speed});
+            this.player.set_position({x: p.x - this.player_speed, y: p.y + this.player_speed});
             return;
         }
-        
-        if(this.is_left)
+
+        if (this.is_left && this.is_up)
         {
             var p = this.player.get_position();
-            this.player.set_position({x:p.x-this.player_speed,y:p.y});
+            this.player.set_position({x: p.x - this.player_speed, y: p.y - this.player_speed});
             return;
         }
-        
-        if(this.is_right)
+
+        if (this.is_down)
         {
             var p = this.player.get_position();
-            this.player.set_position({x:p.x+this.player_speed,y:p.y});
+            this.player.set_position({x: p.x, y: p.y + this.player_speed});
             return;
         }
-        
+
+        if (this.is_up)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x: p.x, y: p.y - this.player_speed});
+            return;
+        }
+
+        if (this.is_left)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x: p.x - this.player_speed, y: p.y});
+            return;
+        }
+
+        if (this.is_right)
+        {
+            var p = this.player.get_position();
+            this.player.set_position({x: p.x + this.player_speed, y: p.y});
+            return;
+        }
+
     };
 
     GameScreen.prototype.show = function() {
